36db558e2864d9614359e7ef28284313eea96fbc
ralf979 <ralf979@gmail.com>
2021-05-11 13:32:14 +0000

    sm8250: Nuke snap camera

--- kona.mk
+++ kona.mk
@@ -92,7 +92,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    Snap \
     vendor.qti.hardware.camera.postproc@1.0.vendor
     
 # Component overrides
