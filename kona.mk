94a29ccac32d6d5a72ad31723f04b7f9b234a406
johnmart19 <johnivan19999@gmail.com>
2021-07-08 01:42:03 +0300

    sm8250: Build offline charging daemon dependency
    This must be explicitly built as of R. Fixes off mode charging.
    
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>

--- kona.mk
+++ kona.mk
@@ -93,6 +93,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
+
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
     
 # Component overrides
 PRODUCT_COPY_FILES += \
