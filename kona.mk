580b19145315f60d217ad179856344bafc497b9e
sukeeratsg <sukeerat@goindi.org>
2021-10-13 18:44:43 +0530

    Havoc : Official

--- kona.mk
+++ kona.mk
@@ -236,7 +236,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-havoc
     
 # OTA
 PRODUCT_HOST_PACKAGES += \
