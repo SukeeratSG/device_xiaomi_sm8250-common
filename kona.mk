fdeadb6c7ce21f3d3a5620c0469a12a865ee7a67
MB <mb150996@gmail.com>
2021-10-14 12:07:40 +0200

    sm8250-common: NetworkStackOverlay defined into source

--- kona.mk
+++ kona.mk
@@ -452,8 +452,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
-    WifiOverlay \
-    NetworkStackOverlay
+    WifiOverlay
 
 # WiFi Display
 PRODUCT_PACKAGES += \
